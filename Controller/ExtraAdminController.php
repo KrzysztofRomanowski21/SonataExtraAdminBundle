57287d28ed9f00b6f7648f09cbefc779508a3da7
KrzysztofRomanowski21 <Krzysztof.Romanowski@eesmith.co.uk>
2026-03-24 16:45:22 +0000

    Update ExtraAdminController.php

--- Controller/ExtraAdminController.php
+++ Controller/ExtraAdminController.php
@@ -206,9 +206,7 @@ class ExtraAdminController extends CRUDController
         ));
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    
     protected function setFormTheme(FormView $formView, $theme)
     {
         $twig = $this->get('twig');
